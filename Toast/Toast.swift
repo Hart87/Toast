eedca323898f353366d1acdf07ebc6b3fb948e21
Miwand <miwand.najafe@gmail.com>
2018-09-29 21:32:51 -0400

    Adding Customization to toast and adding gitIgnore file

--- Toast/Toast.swift
+++ Toast/Toast.swift
@@ -9,40 +9,78 @@
 import Foundation
 import UIKit
 
-class Toast {
+class Toast
+{
+    enum ToastPosition
+    {
+        case top, bottom, center, custom
+    }
     
     var label = UILabel()
     
-    func Show(message:String, textColor:UIColor, backgroundColor:UIColor, view:UIView) {
+    func Show(message: String, toastSettings: ToastSettings = .defaultToastSettings, view: UIView)
+    {
         print("T O A S T")
         let screenSize: CGRect = UIScreen.main.bounds
         
         label.alpha = 0
-        label.textColor = textColor
-        label.backgroundColor = backgroundColor
+        label.textColor = toastSettings.textColor
+        label.backgroundColor = toastSettings.backgroundColor
         label.text = "   " + message + "   "
-        label.font = UIFont(name: "menlo", size: 16)
-        label.textAlignment = .center
+        label.font = toastSettings.font
+        label.textAlignment = toastSettings.textAlignment
         
         //Sizing
-        label.numberOfLines = 0
-        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = toastSettings.numberOfLines
+        label.lineBreakMode = toastSettings.wordWrapping
         label.translatesAutoresizingMaskIntoConstraints = false
         label.sizeToFit()
         
         //Round edges
         label.layer.masksToBounds = true
-        label.layer.cornerRadius = 8.0
+        label.layer.cornerRadius = toastSettings.cornerRadius
         
         //positioning
         label.frame = CGRect(x: 50, y: screenSize.height - 100, width: screenSize.width - 100, height: 75)
         view.addSubview(label)
     
+        self.setToastConstraints(with: view, toastPosition: toastSettings.position, andHeight: toastSettings.toastHeight)
+        
         //Anim8
         UIView.animate(withDuration: 2.5, animations: {
             self.label.alpha = 1
         }) { _ in
-            var timer: Timer! = Timer.scheduledTimer(timeInterval: 4, target: self, selector: #selector(self.removeToast), userInfo: nil, repeats: false)
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + toastSettings.toastDuration, execute: {
+                self.removeToast()
+            })
+        }
+    }
+    
+    private func setToastConstraints(with parentView: UIView, toastPosition: ToastPosition, andHeight height: CGFloat)
+    {
+        DispatchQueue.main.async
+        {
+            self.label.removeConstraints(self.label.constraints)
+            switch toastPosition
+            {
+                case .top:
+                    parentView.topAnchor.constraint(equalTo: self.label.topAnchor, constant: -20).isActive = true
+                    parentView.leadingAnchor.constraint(equalTo: self.label.leadingAnchor, constant: -10).isActive = true
+                    parentView.trailingAnchor.constraint(equalTo: self.label.trailingAnchor, constant: 10).isActive = true
+                case .center:
+                    parentView.leadingAnchor.constraint(equalTo: self.label.leadingAnchor, constant: -10).isActive = true
+                    parentView.trailingAnchor.constraint(equalTo: self.label.trailingAnchor, constant: 10).isActive = true
+                    parentView.centerXAnchor.constraint(equalTo: self.label.centerXAnchor, constant: 0).isActive = true
+                    parentView.centerYAnchor.constraint(equalTo: self.label.centerYAnchor, constant: 0).isActive = true
+                case .bottom:
+                    parentView.bottomAnchor.constraint(equalTo: self.label.bottomAnchor, constant: 10).isActive = true
+                    parentView.leadingAnchor.constraint(equalTo: self.label.leadingAnchor, constant: -10).isActive = true
+                    parentView.trailingAnchor.constraint(equalTo: self.label.trailingAnchor, constant: 10).isActive = true
+                default:
+                    // if user wants to set their own constraints
+                    return
+            }
+            self.label.heightAnchor.constraint(equalToConstant: height).isActive = true
         }
     }
     
@@ -50,8 +88,21 @@ class Toast {
         UIView.animate(withDuration: 2.0, animations: {
             self.label.alpha = 0
         })
-    
     }
     
-    
+    struct ToastSettings
+    {
+        static let defaultToastSettings = ToastSettings(position: .center, toastHeight: 100.0, toastDuration: 4.0, cornerRadius: 8.0, font: UIFont.systemFont(ofSize: 16), textColor: UIColor.white, backgroundColor: UIColor.black, textAlignment: .center, numberOfLines: 0, wordWrapping: .byWordWrapping)
+        
+        let position: ToastPosition
+        let toastHeight: CGFloat
+        let toastDuration: TimeInterval
+        let cornerRadius: CGFloat
+        let font: UIFont
+        let textColor: UIColor
+        let backgroundColor: UIColor
+        let textAlignment: NSTextAlignment
+        let numberOfLines: Int
+        let wordWrapping: NSLineBreakMode
+    }
 }
